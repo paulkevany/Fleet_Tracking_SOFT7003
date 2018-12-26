dbb320a56539bc6603757c5996a55ed2e576ec7b
Paul Kevany <pkevany25@gmail.com>
2018-12-26 20:15:23 +0000

    Added Social Login functionality for Facebook and Google

--- index.php
+++ index.php
@@ -79,6 +79,10 @@
           
           
           <button type="submit" id="button" onclick="signIn();" name="go" class="btn btn-lg btn-primary btn-block">Log in</button>
+		  <br />
+		  <h3 align="center">Social Login</h3>
+          <button type="google" id="googleButton" onclick="googleSignIn();" name="googleBtn" class="btn btn-lg btn-primary btn-block"><img src="images/googleIcon.png" height="40" width="40"> Sign In With Google</button>
+          <button type="facebook" id="facebookButton" onclick="facebookSignIn();" name="facebookBtn" class="btn btn-lg btn-primary btn-block"><img src="images/facebookIcon.png" height="40" width="40"> Facebook Login</button>
 			
 			
 			<script>
@@ -115,6 +119,75 @@
 					});
 					
 				}
+				
+				
+				
+				function googleSignIn(){
+					
+					
+					var provider = new firebase.auth.GoogleAuthProvider(); //Set provider
+					
+					provider.addScope('https://www.googleapis.com/auth/contacts.readonly'); //Set provider scope
+					
+					firebase.auth().languageCode = 'en'; //Set language to user device language
+					
+					firebase.auth().signInWithPopup(provider).then(function(result) {
+  					// This gives you a Google Access Token. You can use it to access the Google API.
+  					var token = result.credential.accessToken;
+  					// The signed-in user info.
+  					var user = result.user;
+  					// ...
+					}).catch(function(error) {
+  					// Handle Errors here.
+  					var errorCode = error.code;
+  					var errorMessage = error.message;
+  					// The email of the user's account used.
+  					var email = error.email;
+  					// The firebase.auth.AuthCredential type that was used.
+  					var credential = error.credential;
+  						// ...
+						});
+					
+				}
+					
+
+
+					
+				
+					
+					
+					function facebookSignIn(){
+						
+					
+						var provider = new firebase.auth.FacebookAuthProvider();
+
+						provider.addScope('user_birthday');
+						
+						firebase.auth().languageCode = 'en_EN';
+						
+						firebase.auth().signInWithPopup(provider).then(function(result) {
+  						// This gives you a Facebook Access Token. You can use it to access the Facebook API.
+  						var token = result.credential.accessToken;
+  						// The signed-in user info.
+  						var user = result.user;
+  						// ...
+						}).catch(function(error) {
+  						// Handle Errors here.
+  						var errorCode = error.code;
+  						var errorMessage = error.message;
+  						// The email of the user's account used.
+  						var email = error.email;
+  						// The firebase.auth.AuthCredential type that was used.
+						var credential = error.credential;
+						// ...
+						});
+
+						
+						
+					}
+				
+				
+				
 			</script>
           
           
